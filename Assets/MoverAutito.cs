1be691f71ef1cbd72a69f2a22b7ac05df30e33cd
DREILING\Usuario <facudreiling8@gmail.com>
2025-02-01 01:33:24 -0300

    epic game

--- Assets/MoverAutito.cs
+++ Assets/MoverAutito.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class MoverAutito : MonoBehaviour
 {
+    public float carSpeed;
+    public Rigidbody car;
+    public AudioSource carSound;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,13 @@ public class MoverAutito : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            car.AddForce(Vector3.right * -carSpeed, ForceMode.Acceleration);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            carSound.Play();
+        }
     }
 }
