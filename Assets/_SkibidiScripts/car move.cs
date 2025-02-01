b894f6dc1e0a1a253213dd075cf349230dafeb86
TMan <tomasmedinaboriglio@gmail.com>
2025-02-01 16:43:23 -0300

    skibidi ultrakrill

--- Assets/_SkibidiScripts/car move.cs
+++ Assets/_SkibidiScripts/car move.cs
@@ -15,14 +15,26 @@ public class carmove : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.W))
         {
-            rb.AddTorque(transform.forward.x * 10, transform.forward.y * 10, transform.forward.z * 10, ForceMode.Force);
+            rb.AddForce(-transform.right * 10, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddTorque(-transform.right * 10, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(transform.right * 10, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddTorque(transform.right * 10, ForceMode.Force);
         }
         CamHandle();
     }
     void CamHandle()
     {
         cameraParentGO.transform.position = transform.position;
-        cameraGO.transform.rotation = Quaternion.Euler(10, transform.rotation.y -90, 0);
-        cameraGO.transform.rotation = Quaternion.Euler(10, transform.rotation.y - 90, 0);
+        cameraGO.transform.rotation = Quaternion.Euler(10, cameraGO.transform.rotation.eulerAngles.y, 0);
+        cameraParentGO.transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y + 90, 0);
     }
 }
