f3ec071af5fd2701399657dea67978a2c7820cfd
TMan <tomasmedinaboriglio@gmail.com>
2025-02-01 01:19:12 -0300

    dafhji

--- Assets/MoverAutito.cs
+++ Assets/MoverAutito.cs
@@ -5,12 +5,12 @@ public class MoverAutito : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Destroy(gameObject);
     }
 }
