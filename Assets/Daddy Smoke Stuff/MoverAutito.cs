01f7df617b0fb7053ebc9e78871982b7e91c7e8e
DREILING\Usuario <facudreiling8@gmail.com>
2025-02-01 01:58:53 -0300

    todo esto es mio perras

--- Assets/Daddy Smoke Stuff/MoverAutito.cs
+++ Assets/Daddy Smoke Stuff/MoverAutito.cs
@@ -9,17 +9,35 @@ public class MoverAutito : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Destroy(gameObject);
+
     }
 
     // Update is called once per frame
     void Update()
+    {
+        GoForward();
+        GoBack();
+    }
+
+    void GoForward()
     {
         if (Input.GetKey(KeyCode.W))
         {
             car.AddForce(Vector3.right * -carSpeed, ForceMode.Acceleration);
         }
         if (Input.GetKeyDown(KeyCode.W))
+        {
+            carSound.Play();
+        }
+    }
+
+    void GoBack()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            car.AddForce(Vector3.right * carSpeed, ForceMode.Acceleration);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
         {
             carSound.Play();
         }
